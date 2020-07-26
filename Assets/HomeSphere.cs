80e3b96621f6412a9e9939dbbe63d4a03fbf0aa9
August Alexandersson <augustalexandersson@gmail.com>
2020-07-26 19:58:36 +0200

    ...

--- Assets/HomeSphere.cs
+++ Assets/HomeSphere.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 
 public class HomeSphere : MonoBehaviour
 {
+    public AudioClip recenterSound;
     public GameObject camera;
+    private float _enterTime;
 
     private void OnTriggerEnter(Collider other)
     {
-        var followObjectComponent = other.GetComponent<FollowObject>();
-        var isSomeInteractionObject = followObjectComponent != null && followObjectComponent.enabled;
-        if (isSomeInteractionObject)
+        _enterTime = Time.fixedTime;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var delta = Time.fixedTime - _enterTime;
+        if (delta > 2)
         {
-            camera.transform.position = new Vector3(0, camera.transform.position.y, 0);
-            var diff = Camera.main.transform.position - camera.transform.position;
-            diff.y = 0;
-            camera.transform.position -= diff;
+            var followObjectComponent = other.GetComponent<FollowObject>();
+            var isSomeInteractionObject = followObjectComponent != null && followObjectComponent.enabled;
+            if (isSomeInteractionObject)
+            {
+                camera.transform.position = new Vector3(0, camera.transform.position.y, 0);
+                var diff = Camera.main.transform.position - camera.transform.position;
+                diff.y = 0;
+                camera.transform.position -= diff;
+                GetComponent<AudioSource>().PlayOneShot(recenterSound);
+            }
         }
     }
 }
