be08f8935e61a709ebee675eb3ae30ecca778c0b
August Alexandersson <augustalexandersson@gmail.com>
2020-07-26 04:12:42 +0200

    ...

---
+++ Assets/GreensSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreensSpawn : MonoBehaviour
+{
+    public GameObject[] greensTemplates;
+
+    void Awake()
+    {
+        var greensTemplate = greensTemplates[Random.Range(0, greensTemplates.Length)];
+        var greens = Instantiate(greensTemplate);
+        greens.transform.SetParent(transform, false);
+        greens.transform.Rotate(new Vector3(0, Random.value * 360, 0));
+    }
+}
---
+++ Assets/PlaceGreensBlockInteractor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceGreensBlockInteractor : BlockInteractor
+{
+    public GameObject greensBlockTemplate;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!IsActivated()) return;
+
+        var blockComponent = other.gameObject.GetComponent<Block>();
+        if (blockComponent && blockComponent.IsInteractable() && blockComponent.blockType == Block.BlockType.Grass && blockComponent.Vacant())
+        {
+            var greens = Instantiate(greensBlockTemplate);
+            blockComponent.Occupy(greens);
+            blockComponent.ShortFreeze();
+            PlayGeneralSound();
+
+            StopRigidbodySoon(greens);
+        }
+    }
+    
+    public void StopRigidbodySoon(GameObject greens)
+    {
+        StartCoroutine(DoSoon());
+
+        IEnumerator DoSoon()
+        {
+            yield return new WaitForSeconds(2);
+
+            greens.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+}
