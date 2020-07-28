7e5e538ed015bf687e0ff723aa75632084edd012
August Alexandersson <augustalexandersson@gmail.com>
2020-07-28 23:39:32 +0200

    Now upgrade houses to large houses when many small houses are nearby

--- Assets/HouseSpawn.cs
+++ Assets/HouseSpawn.cs
@@ -5,11 +5,34 @@ using UnityEngine;
 public class HouseSpawn : MonoBehaviour
 {
     public GameObject[] tinyHouseTemplates;
+    public GameObject[] largeHouseTemplates;
+
+    private GameObject _activeHouse;
+    private int _size;
 
     void Awake()
     {
         var houseTemplate = tinyHouseTemplates[Random.Range(0, tinyHouseTemplates.Length)];
         var house = Instantiate(houseTemplate);
         house.transform.SetParent(transform, false);
+
+        _activeHouse = house;
+        _size = 0;
+    }
+
+    public void Upgrade()
+    {
+        Destroy(_activeHouse);
+        
+        var houseTemplate = largeHouseTemplates[Random.Range(0, largeHouseTemplates.Length)];
+        var house = Instantiate(houseTemplate);
+        house.transform.SetParent(transform, false);
+
+        _size = 1;
+    }
+
+    public bool IsLarge()
+    {
+        return _size == 1;
     }
 }
