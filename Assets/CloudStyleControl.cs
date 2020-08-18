0e5f250b334d5e087e3fbc7527e1cfdf3b76a7d4
August Alexandersson <augustalexandersson@gmail.com>
2020-08-18 20:06:09 +0200

    Cloud now has a chance to become a storm cloud!

---
+++ Assets/CloudStyleControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudStyleControl : MonoBehaviour
+{
+    public Material normalMaterial;
+    public Material stormMaterial;
+
+    public void SetNormalMaterial()
+    {
+        gameObject.GetComponentInChildren<MeshRenderer>().material = normalMaterial;
+    }
+
+    public void SetStormMaterial()
+    {
+        gameObject.GetComponentInChildren<MeshRenderer>().material = stormMaterial;
+    }
+}
--- Assets/RainSpawn.cs
+++ Assets/RainSpawn.cs
@@ -1,14 +1,51 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RainSpawn : MonoBehaviour
 {
     public GameObject dropletTemplate;
+    public GameObject stormTemplate;
+
+    private bool _storm = true;
+    private CloudStyleControl _cloudStyleControl;
+
+    private void Awake()
+    {
+        _cloudStyleControl = GetComponentInParent<CloudStyleControl>();
+    }
+
     void Update()
     {
-        if (Random.value < .5f)
+        if (Random.value < .001f)
         {
-            SpawnDroplet(RandomLocationInArea());
+            _storm = !_storm;
+            if (_storm)
+            {
+                _cloudStyleControl.SetStormMaterial();
+            }
+            else
+            {
+                _cloudStyleControl.SetNormalMaterial();
+            }
         }
+        
+        if (_storm)
+        {
+            SpawnStormDroplet(RandomLocationInArea());
+        }
+        else
+        {
+            if (Random.value < .5f)
+            {
+                SpawnDroplet(RandomLocationInArea());
+            }
+        }
+    }
+
+    private void SpawnStormDroplet(Vector3 spawnPosition)
+    {
+        Instantiate(stormTemplate, spawnPosition, Quaternion.identity);
     }
 
     private void SpawnDroplet(Vector3 spawnPosition)
@@ -22,15 +59,15 @@ public class RainSpawn : MonoBehaviour
         var scale = transform.localScale;
         var originalX = position.x;
         var originalZ = position.z;
-        
+
         var minX = originalX - scale.x * .5f;
         var maxX = originalX + scale.x * .5f;
         var minZ = originalZ - scale.z * .5f;
         var maxZ = originalZ + scale.z * .5f;
-        
+
         var x = Random.Range(minX, maxX);
         var z = Random.Range(minZ, maxZ);
-        
+
         return new Vector3(x, position.y, z);
     }
 }
