929fe83b73b19c97e816c6d7c1f9c804c1c9dee7
August Alexandersson <augustalexandersson@gmail.com>
2020-09-13 15:23:40 +0200

    Started work on moving cloud with pinch system

--- Assets/Cloud.cs
+++ Assets/Cloud.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Cloud : MonoBehaviour
 {
+    private Transform _followPoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,24 @@ public class Cloud : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_followPoint)
+        {
+            var newPosition = new Vector3(_followPoint.position.x, transform.position.y, _followPoint.position.z);
+            transform.position = newPosition;
+        }
+    }
+
+    public void Hook(Transform followPoint)
+    {
+        _followPoint = followPoint;
+    }
+
+    public void UnHook(Transform referenceFollowPoint)
+    {
+        if (_followPoint == referenceFollowPoint)
+        {
+            _followPoint = null;
+            GetComponentInParent<Rigidbody>().AddForce(transform.gameObject.GetComponent<Rigidbody>().velocity, ForceMode.VelocityChange);
+        }
     }
 }
---
+++ Assets/PrimaryGrabber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PrimaryGrabber : MonoBehaviour
+{
+    private Cloud _nearbyCloud;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var isSecondaryGrabber = other.gameObject.GetComponent<SecondaryGrabber>();
+        if (isSecondaryGrabber)
+        {
+            var cloud = GetNearbyCloudHook();
+            if (cloud != null)
+            {
+                _nearbyCloud = cloud;
+                GrabCloud();
+            }
+        }
+    }
+
+    private void GrabCloud()
+    {
+        _nearbyCloud.Hook(transform);
+    }
+
+    private Cloud GetNearbyCloudHook()
+    {
+        var hits = Physics.OverlapSphere(transform.position,  .2f);
+        return hits
+            .FirstOrDefault(hit => hit.gameObject.GetComponent<Cloud>() != null)
+            ?.gameObject .GetComponent<Cloud>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var isSecondaryGrabber = other.gameObject.GetComponent<SecondaryGrabber>();
+        if (isSecondaryGrabber)
+        {
+            ReleaseCloud();
+        }
+    }
+
+    private void ReleaseCloud()
+    {
+        _nearbyCloud.UnHook(transform);
+    }
+}
