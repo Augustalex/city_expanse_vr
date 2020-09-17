6c280f828323a5f9e1e602aad1f15808b2d81859
August Alexandersson <augustalexandersson@gmail.com>
2020-09-17 22:31:05 +0200

    Improved Palette interaction

---
+++ Assets/Scripts/ClosedFistDetector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosedFistDetector : MonoBehaviour
+{
+    public Transform palmTransform;
+    private bool _closed;
+    
+    public event Action OpenFist;
+    public event Action CloseFist;
+
+    void Update()
+    {
+        transform.position = palmTransform.position;
+    }
+
+    public bool IsOpen()
+    {
+        return !_closed;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        var isMiddleFinger = other.CompareTag("MiddleFingerTracker");
+        if (isMiddleFinger)
+        {
+            _closed = true;
+            OnCloseFist();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var isMiddleFinger = other.CompareTag("MiddleFingerTracker");
+        if (isMiddleFinger)
+        {
+            _closed = false;
+            OnOpenFist();
+        }
+    }
+
+    private void OnOpenFist()
+    {
+        OpenFist?.Invoke();
+    }
+
+    private void OnCloseFist()
+    {
+        CloseFist?.Invoke();
+    }
+}
--- Assets/Scripts/FollowHead.cs
+++ Assets/Scripts/FollowHead.cs
@@ -6,10 +6,33 @@ using UnityEngine;
 public class FollowHead : MonoBehaviour
 {
     public Transform followPoint;
+    public ClosedFistDetector closedFistDetector;
+
+    private bool _paletteInSight;
+    private BlockInteractionPalette _activePalette;
     
     void Update()
     {
         transform.SetPositionAndRotation(followPoint.position, followPoint.rotation);
+        
+        closedFistDetector.OpenFist += ShowActivePalette;
+        closedFistDetector.CloseFist += HideActivePalette;
+    }
+
+    private void ShowActivePalette()
+    {
+        if (_paletteInSight)
+        {
+            _activePalette.Show();
+        }
+    }
+    
+    private void HideActivePalette()
+    {
+        if (_paletteInSight)
+        {
+            _activePalette.Hide();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -17,7 +40,13 @@ public class FollowHead : MonoBehaviour
         var palette = other.GetComponent<BlockInteractionPalette>();
         if (palette)
         {
-            palette.Show();
+            _activePalette = palette;
+            _paletteInSight = true;
+            
+            if (closedFistDetector.IsOpen())
+            {
+                palette.Show();
+            }
         }
     }
 
@@ -26,6 +55,9 @@ public class FollowHead : MonoBehaviour
         var palette = other.GetComponent<BlockInteractionPalette>();
         if (palette)
         {
+            _activePalette = null;
+            _paletteInSight = false;
+            
             palette.Hide();
         }
     }
