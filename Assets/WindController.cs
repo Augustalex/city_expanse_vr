f9b756883f4f562f64570e09cd50a4460731b741
August Alexandersson <augustalexandersson@gmail.com>
2020-09-20 13:57:17 +0200

    Added wind turbines

---
+++ Assets/WindController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Oculus.Platform.Models;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class WindController : MonoBehaviour
+{
+    private Quaternion _windDirection;
+    private float _windForce = 10;
+    private int _turningRate = 1;
+    private static WindController _instance;
+
+    void Awake()
+    {
+        _windDirection = Quaternion.Euler(0, Random.value, 0);
+        _instance = FindObjectOfType<WindController>();
+    }
+
+    void Update()
+    {
+        if (Random.value < .0001f)
+        {
+            _turningRate = -_turningRate;
+            _windForce = Random.Range(1, 5);
+        }
+
+        var turn = _turningRate * Time.deltaTime * _windForce;
+        _windDirection = Quaternion.Euler(_windDirection.eulerAngles + new Vector3(0, turn, 0));
+    }
+
+    public Quaternion GetWindDirection()
+    {
+        return _windDirection;
+    }
+
+    public static WindController Get()
+    {
+        if (_instance == null)
+        {
+            throw new Exception("No WindController active in the scene while some object is trying to get it.");
+        }
+
+        return _instance;
+    }
+}
---
+++ Assets/WindTurbine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindTurbine : MonoBehaviour
+{
+    private WindController _windController;
+
+    void Start()
+    {
+        _windController = WindController.Get();
+    }
+
+    void Update()
+    {
+        transform.rotation = _windController.GetWindDirection();
+    }
+}
