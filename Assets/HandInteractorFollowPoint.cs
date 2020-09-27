b73e540972e2db5f6d293704a845aaeee4a233a6
August Alexandersson <augustalexandersson@gmail.com>
2020-09-27 17:29:05 +0200

    Fixed issue with 2D palette

---
+++ Assets/HandInteractorFollowPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class HandInteractorFollowPoint : MonoBehaviour
+{
+    public Transform FollowPoint;
+    
+    private HandInteractorFollowPoint _instance;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public HandInteractorFollowPoint Get()
+    {
+        return _instance;
+    }
+}
