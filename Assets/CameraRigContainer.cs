888380bc8b6838a620896063c46d166802af3cd8
August Alexandersson <augustalexandersson@gmail.com>
2020-10-18 19:32:15 +0200

    Add recenter icon

---
+++ Assets/CameraRigContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRigContainer : MonoBehaviour
+{
+    private static CameraRigContainer _instance;
+
+    public GameObject cameraRig;
+    
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public static CameraRigContainer Get()
+    {
+        return _instance;
+    }
+}
--- Assets/Scripts/HomeSphere.cs
+++ Assets/Scripts/HomeSphere.cs
@@ -6,8 +6,13 @@ using UnityEngine;
 public class HomeSphere : MonoBehaviour
 {
     public AudioClip recenterSound;
-    public GameObject camera;
+    private GameObject _camera;
     private float _enterTime;
+
+    private void Awake()
+    {
+        _camera = CameraRigContainer.Get().cameraRig;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,16 +22,20 @@ public class HomeSphere : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         var delta = Time.fixedTime - _enterTime;
-        if (delta > 1)
+        if (delta > .1f)
         {
             var blockInteractor = other.GetComponent<BlockInteractor>();
             var isSomeInteractionObject = blockInteractor != null && blockInteractor.IsActivated();
             if (isSomeInteractionObject)
             {
-                camera.transform.position = new Vector3(0, camera.transform.position.y, 0);
-                var diff = Camera.main.transform.position - camera.transform.position;
+                _camera.transform.position = new Vector3(0, _camera.transform.position.y, 0);
+                var diff = Camera.main.transform.position - _camera.transform.position;
                 diff.y = 0;
-                camera.transform.position -= diff;
+                _camera.transform.position -= diff;
+
+                var currentPosition = _camera.transform.position;
+                _camera.transform.position = new Vector3(currentPosition.x, .6f, currentPosition.z);
+
                 GetComponent<AudioSource>().PlayOneShot(recenterSound);
             }
         }
