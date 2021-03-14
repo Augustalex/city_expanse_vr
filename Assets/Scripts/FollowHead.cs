a0ab44e0a46bcf213c1752ab1c3ed80daf1661ad
August Alexandersson <augustalexandersson@gmail.com>
2021-03-14 15:26:15 +0100

    Fixed incorrect adding of events inside update loop

--- Assets/Scripts/FollowHead.cs
+++ Assets/Scripts/FollowHead.cs
@@ -10,13 +10,16 @@ public class FollowHead : MonoBehaviour
 
     private bool _paletteInSight;
     private BlockInteractionPalette _activePalette;
+
+    void Awake()
+    {
+        closedFistDetector.OpenFist += ShowActivePalette;
+        closedFistDetector.CloseFist += HideActivePalette;
+    }
     
     void Update()
     {
         transform.SetPositionAndRotation(followPoint.position, followPoint.rotation);
-        
-        closedFistDetector.OpenFist += ShowActivePalette;
-        closedFistDetector.CloseFist += HideActivePalette;
     }
 
     private void ShowActivePalette()
