4e067559610b9f5030789233e3b5304b67c480e2
August Alexandersson <augustalexandersson@gmail.com>
2021-05-16 16:23:08 +0200

    Can now build inner city houses

--- Assets/Scripts/ConstructHouseBlockInteractor.cs
+++ Assets/Scripts/ConstructHouseBlockInteractor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -27,11 +28,11 @@ public class ConstructHouseBlockInteractor : BlockInteractor
     public override void Interact(GameObject other)
     {
         var vacantLot = other.gameObject.GetComponent<Block>();
-        
+
         var candidates = GetWorldPlane().GetNearbyBlocks(vacantLot.GetGridPosition())
             .Where(otherBlock => otherBlock.IsWater() && otherBlock.IsOnSameHeightAs(vacantLot))
             .ToList();
-        
+
         var candidatesCount = candidates.Count;
         if (candidatesCount > 0)
         {
@@ -41,8 +42,33 @@ public class ConstructHouseBlockInteractor : BlockInteractor
             var target = waterBlock.transform.position;
             target.y = house.transform.position.y;
             house.transform.LookAt(target);
-            
+
             PlaySound(BlockSoundLibrary.BlockSound.RaiseLand);
+        }
+        else
+        {
+            SpawnInnerCityHouse(other);
+        }
+    }
+
+    private void SpawnInnerCityHouse(GameObject other)
+    {
+        var vacantLot = other.gameObject.GetComponent<Block>();
+
+        var candidates = GetWorldPlane().GetNearbyBlocks(vacantLot.GetGridPosition())
+            .Where(otherBlock => otherBlock.OccupiedByHouse() && otherBlock.GetOccupantHouse().IsBig())
+            .ToList();
+
+        if (candidates.Count > 0)
+        {
+            var bigHouse = candidates.ElementAt(Random.Range(0, candidates.Count));
+            var house = Instantiate(tinyHouseTemplate);
+            house.GetComponent<HouseSpawn>().SetIsInnerCity();
+            vacantLot.Occupy(house);
+
+            var target = bigHouse.transform.position;
+            target.y = house.transform.position.y;
+            house.transform.LookAt(target);
         }
     }
 }
