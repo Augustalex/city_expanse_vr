f0517e21481bf3ee112ba3fa254ed8e027a8d038
August Alexandersson <augustalexandersson@gmail.com>
2021-06-19 20:05:30 +0200

    Added day night cycle

---
+++ Assets/DayCycle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCycle : MonoBehaviour
+{
+    public GameObject sun;
+    public GameObject pivotPoint;
+    public GameObject sunLightSource;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        sun.transform.RotateAround(pivotPoint.transform.position, Vector3.forward + Vector3.left, Time.deltaTime * 10f);
+        sunLightSource.transform.position = sun.transform.position;
+        sunLightSource.transform.LookAt(pivotPoint.transform);
+    }
+}
