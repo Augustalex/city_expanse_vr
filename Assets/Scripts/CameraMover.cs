69a2d4848693ac092aabcd8916ee93abc6f47cdc
August Alexandersson <augustalexandersson@gmail.com>
2021-07-12 14:53:20 +0200

    Enabled rotation for all camera movement modes

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraMover : MonoBehaviour
 {
+    private const float CameraYMax = 5f;
+    private const float CameraYMin = 1.4f;
+    private const float CameraXMin = -2f;
+    private const float CameraXMax = 4f;
+    private const int CameraZMin = -3;
+    private const int CameraZMax = 3;
     private Camera _camera;
-    private bool _rightDown;
+    private bool _shouldFireCameraInTargetDirection;
     private Vector3 _startMovePosition;
     private Vector3 _startCameraPosition;
     private bool _leftDown;
@@ -18,22 +24,32 @@ public class CameraMover : MonoBehaviour
 
     void Update()
     {
+        HandleZoomTilt();
+        HandleKeyboardMovement();
+        HandleMousePanMovement();
+        HandleRightDragMovement();
+        ClampCameraPosition();
+    }
+
+    private void ClampCameraPosition()
+    {
+        var newPosition = _camera.transform.position;
+        _camera.transform.position = new Vector3(
+            Mathf.Clamp(newPosition.x, CameraXMin, CameraXMax),
+            Mathf.Clamp(newPosition.y, CameraYMin, CameraYMax),
+            Mathf.Clamp(newPosition.z, CameraZMin, CameraZMax)
+        );        
+    }
+
+    private void HandleZoomTilt()
+    {
         var change = Input.mouseScrollDelta.y;
         _camera.transform.position += new Vector3(0, change * .1f, 0);
-
-        var cameraYMax = 5f;
-        var cameraYMin = 1.4f;
-
-        var cameraXMin = -2f;
-        var cameraXMax = 4f;
-        
-        var cameraZMin = -3;
-        var cameraZMax = 3;
 
         if (change > 0 || change < 0)
         {
             var newYPosition = _camera.transform.position.y;
-            var zoomProgress = Mathf.Clamp((newYPosition - cameraYMin) / (cameraYMax - cameraYMin), 0f, 1f);
+            var zoomProgress = Mathf.Clamp((newYPosition - CameraYMin) / (CameraYMax - CameraYMin), 0f, 1f);
             var easedZoomProgress = EaseOutExpo(zoomProgress);
 
             var minTilt = 4f;
@@ -44,44 +60,12 @@ public class CameraMover : MonoBehaviour
             if (currentTilt + differenceOfTilt <= maxTilt || currentTilt + differenceOfTilt >= minTilt)
             {
                 _camera.transform.RotateAround(_camera.transform.position, _camera.transform.right, differenceOfTilt);
-                     
             }
         }
-
-
-        const float cameraMovementSpeed = .75f;
-        var direction = Vector3.zero;
-        if (Input.GetKey(KeyCode.W))
-        {
-            direction += Vector3.left;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            direction += Vector3.back;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            direction += Vector3.right;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            direction += Vector3.forward;
-        }
-
-        _camera.transform.position += direction * (cameraMovementSpeed * Time.deltaTime);
-        
-        // var rotation = 0;
-        // if (Input.GetKey(KeyCode.Q))
-        // {
-        //     rotation -= 1;
-        // }
-        // else if (Input.GetKey(KeyCode.E))
-        // {
-        //     rotation += 1;
-        // }
-        //
-        // _camera.transform.RotateAround(_camera.transform.position, Vector3.up, rotation * 40 * Time.deltaTime);
-
+    }
+    
+    private void HandleMousePanMovement()
+    {
         if (Input.GetKey(KeyCode.LeftControl) && Input.GetMouseButton(0))
         {
             if (!_leftDown)
@@ -92,27 +76,66 @@ public class CameraMover : MonoBehaviour
             else
             {
                 var endPosition = Input.mousePosition;
-                var movementDelta = (endPosition - _startLeftMouseButtonMovePosition);
+                var mouseDirection = (endPosition - _startLeftMouseButtonMovePosition).normalized;
 
-                var progress = movementDelta.magnitude / (_startLeftMouseButtonMovePosition + new Vector3(250, 0, 250) -
-                                                          _startLeftMouseButtonMovePosition).magnitude;
-                var curvedProgress = progress;
-                var speed = .01f * Mathf.Clamp(curvedProgress, 0, 1);
-
-                _camera.transform.position += new Vector3(movementDelta.y, 0, -movementDelta.x).normalized * -speed;
+                var curvedProgress = (endPosition - _startLeftMouseButtonMovePosition).magnitude / 250;
+                var speed = .025f * Mathf.Clamp(curvedProgress, 0, 1);
+                    
+                var newDirection = _camera.transform.rotation * mouseDirection * speed;
+                newDirection.y = 0;
+                
+                _camera.transform.position += newDirection;
             }
-
+        
             _leftDown = true;
         }
-
+        
         if (!Input.GetMouseButton(0))
         {
             _leftDown = false;
         }
+    }
 
+    private void HandleKeyboardMovement()
+    {
+        const float cameraMovementSpeed = .75f;
+        var direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += _camera.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += -_camera.transform.right;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += -_camera.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += _camera.transform.right;
+        }
+        direction = new Vector3(direction.x, 0, direction.z).normalized;
+        _camera.transform.position += direction * (cameraMovementSpeed * Time.deltaTime);
+        
+        var rotation = 0;
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotation -= 1;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            rotation += 1;
+        }
+        _camera.transform.RotateAround(_camera.transform.position, Vector3.up, rotation * 40 * Time.deltaTime);
+    }
+
+    private void HandleRightDragMovement()
+    {
         if (Input.GetMouseButton(1))
         {
-            if (!_rightDown)
+            if (!_shouldFireCameraInTargetDirection)
             {
                 _startCameraPosition = _camera.transform.position;
                 _startMovePosition = Input.mousePosition;
@@ -121,47 +144,37 @@ public class CameraMover : MonoBehaviour
             else
             {
                 var endPosition = Input.mousePosition;
-                var movementDelta = (endPosition - _startMovePosition);
+                var mouseDirection = (endPosition - _startMovePosition).normalized;
+                
+                var easedVelocity = QuarticEaseIn((endPosition - _startMovePosition).magnitude / 250);
+                var speed = .25f * Mathf.Clamp(easedVelocity, 0, 1);
 
-                var progress = movementDelta.magnitude /
-                               (_startMovePosition + new Vector3(250, 0, 250) - _startMovePosition).magnitude;
-                var curvedProgress = QuarticEaseIn(progress);
-                var speed = .05f * Mathf.Clamp(curvedProgress, 0, 1);
-
-
-                // _camera.transform.position = _startCameraPosition + _camera.transform.forward * (movementDelta.y * -speed) +
-                                             // _camera.transform.right * (-movementDelta.x * -speed);  
-                _camera.transform.position = _startCameraPosition +
-                                             new Vector3(movementDelta.y, 0, -movementDelta.x).normalized * -speed;
+                var newDirection = _camera.transform.rotation * mouseDirection * speed;
+                newDirection.y = 0;
+                
+                _camera.transform.position = _startCameraPosition + newDirection * -speed;
             }
 
-            _rightDown = true;
+            _shouldFireCameraInTargetDirection = true;
         }
         else
         {
-            if (_rightDown)
+            if (_shouldFireCameraInTargetDirection)
             {
                 var endPosition = Input.mousePosition;
-                var movementDelta = (endPosition - _startMovePosition);
+                var mouseDirection = (endPosition - _startMovePosition).normalized;
+                
+                var easedVelocity = (endPosition - _startMovePosition).magnitude / 250;
+                var speed = 2 * Mathf.Clamp(easedVelocity, 0, 1);
 
-                var progress = movementDelta.magnitude /
-                               (_startMovePosition + new Vector3(250, 0, 250) - _startMovePosition).magnitude;
-                var curvedProgress = progress;
-                var speed = .02f * Mathf.Clamp(curvedProgress, 0, 1);
-
-                _camera.GetComponent<Rigidbody>().AddForce(new Vector3(movementDelta.y, 0, -movementDelta.x) * speed,
-                    ForceMode.Impulse);
+                var newDirection = _camera.transform.rotation * -mouseDirection * speed;
+                newDirection.y = 0;
+                
+                _camera.GetComponent<Rigidbody>().AddForce( newDirection, ForceMode.Impulse); 
             }
 
-            _rightDown = false;
+            _shouldFireCameraInTargetDirection = false;
         }
-
-        var newPosition = _camera.transform.position;
-        _camera.transform.position = new Vector3(
-            Mathf.Clamp(newPosition.x, cameraXMin, cameraXMax),
-            Mathf.Clamp(newPosition.y, cameraYMin, cameraYMax),
-            Mathf.Clamp(newPosition.z, cameraZMin, cameraZMax)
-        );
     }
 
     private float EaseOutExpo(float x)
