f94e9fe2593392a54b7978f7f6d17f87e26be034
August Alexandersson <augustalexandersson@gmail.com>
2021-08-05 11:04:23 +0200

    Updated post processing effects

---
+++ Assets/PostProcessingController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class PostProcessingController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        var volume = GetComponent<Volume>();
+        DepthOfField dof;
+        if (volume.profile.TryGet(out dof))
+        {
+            Debug.Log("UPDATING!");
+            dof.focalLength = new ClampedFloatParameter(10f, 10f, 10f, true);
+        }
+    }
+}
