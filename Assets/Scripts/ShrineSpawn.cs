5dfb652fe0690d5f5f5f0265949e04aa76d280a0
August Alexandersson <augustalexandersson@gmail.com>
2021-12-31 15:27:28 +0100

    Fixed shrine spawn

--- Assets/Scripts/ShrineSpawn.cs
+++ Assets/Scripts/ShrineSpawn.cs
@@ -5,53 +5,31 @@ using UnityEngine;
 public class ShrineSpawn : MonoBehaviour
 {
     public GameObject[] shrineTemplates;
-    public AudioClip upgradeSound;
-    
-    private FireworksEffect _fireworksEffect;
-    
+
     private static int _shrineCount = 0;
 
     void Awake()
     {
-        _fireworksEffect = GetComponentInChildren<FireworksEffect>();
-        _fireworksEffect.ActivatedHit += PlayUpgradeSound;
-        
         SetupHouse(shrineTemplates);
-        _fireworksEffect.SetHitBoxSize(5);
-        
-        StartCoroutine(ActivateFireworksSoon());
-        LaunchFromAbove();
-
-        IEnumerator ActivateFireworksSoon()
-        {
-            yield return new WaitForSeconds(.05f);
-            _fireworksEffect.Activate();
-        }
     }
 
     void Start()
     {
         if (_shrineCount == 0)
         {
-            DiscoveryManager.Get().RegisterNewDiscover(DiscoveryManager.Discoverable.ForrestShrine);
+            var discoveryManager = DiscoveryManager.Get();
+            if (discoveryManager)
+            {
+                discoveryManager.RegisterNewDiscover(DiscoveryManager.Discoverable.ForrestShrine);
+            }
         }
 
         _shrineCount += 1;
     }
-    
+
     private void SetupHouse(GameObject[] templates)
     {
         var houseTemplate = templates[Random.Range(0, templates.Length)];
         Instantiate(houseTemplate, transform, false);
-    }
-    
-    private void PlayUpgradeSound()
-    {
-        GetComponent<AudioSource>().PlayOneShot(upgradeSound, .015f * GameManager.MasterVolume);
-    }
-    
-    private void LaunchFromAbove()
-    {
-        GetComponent<Rigidbody>().AddForce(Vector3.up * 2, ForceMode.Impulse);
     }
 }
---
+++ Assets/ShrineTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrineTest : MonoBehaviour
+{
+    public GameObject grassBlockTemplate;
+    public GameObject houseSpawnTemplate;
+    private int _step;
+    private Block _block;
+
+    private void Start()
+    {
+        var grassBlockParent = Instantiate(grassBlockTemplate);
+        _block = grassBlockParent.GetComponentInChildren<Block>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PerformTest();
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            TearDown();
+        }
+    }
+
+    private void TearDown()
+    {
+        _block.DestroyOccupant();
+        
+        _step = 0;
+    }
+
+    private void PerformTest()
+    {
+        if (_step == 0)
+        {
+            var houseSpawn = Instantiate(houseSpawnTemplate);
+            _block.Occupy(houseSpawn);
+        }
+
+        _step += 1;
+    }
+}
